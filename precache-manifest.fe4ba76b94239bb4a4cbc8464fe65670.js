a7a336f8a1c1bac6fb82d3d9850ee76c11285103
Yemi Fakorede <yemifakorede@gmail.com>
2020-07-13 14:20:26 -0400

    Updates

---
+++ precache-manifest.fe4ba76b94239bb4a4cbc8464fe65670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227c6932abe70a159c035ed3171a91d9",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "5ca598f76c124a44fd88",
+    "url": "/react-todo-list/static/css/main.7cee5cc3.chunk.css"
+  },
+  {
+    "revision": "c9a0702ccf294ae04328",
+    "url": "/react-todo-list/static/js/2.df7096c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.df7096c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca598f76c124a44fd88",
+    "url": "/react-todo-list/static/js/main.32b19838.chunk.js"
+  },
+  {
+    "revision": "805eaab7b9c5c346c016",
+    "url": "/react-todo-list/static/js/runtime-main.fcf2421e.js"
+  }
+]);
